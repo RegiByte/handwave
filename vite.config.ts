6ea521ae31369a2ec11db34a88ae3c032ea4852b
Reginaldo Junior <reginaldo.junior@ae.studio>
2026-01-20 12:21:37 -0300

    feat: cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import path from 'node:path'
 
 const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     devtools(),
-    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
@@ -19,7 +23,10 @@ const config = defineConfig({
       spa: {
         enabled: true,
       },
-      
+      prerender: {
+        enabled: false
+      }
+
     }),
     viteReact(),
   ],
